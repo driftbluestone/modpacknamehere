5d37c0b0eee49444ea38b39172c40e067db514cb
driftbluestone <117239036+driftbluestone@users.noreply.github.com>
2024-03-09 00:12:07 -0800

    removed some shop tabs, added coins (no textures yet). added some shopping "quests"

---
+++ kubejs/startup_scripts/coins.js
@@ -0,0 +1,6 @@
+StartupEvents.registry('item', event => {
+    event.create('pre_elec_coin').displayName(`Pre-Electrical Pennies`)
+    event.create('electrical_coin').displayName(`Electrical Euros`)
+    event.create('ae2_coin').displayName(`ae2 coin (TEMP NAME)`)
+    event.create('magic_coin').displayName(`Magical Money`)
+});
